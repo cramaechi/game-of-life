e5315cb52751c83a2d009e36c75401f0920991ff
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 11:17:33 -0600

    Modified .main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -51,119 +51,119 @@ void nextGeneration(char world[][COLUMN]);
 
 int main()
 {
-	char world[ROW][COLUMN];
-	
-	createWorld(world);
+    char world[ROW][COLUMN];
+
+    createWorld(world);
     char next;
 
-	do
-	{
-		nextGeneration(world);
-		cout<<"Press Return to continue or q to quit: ";
+    do
+    {
+        nextGeneration(world);
+        cout<<"Press Return to continue or q to quit: ";
         cin.get(next);
         cout<<endl;
         if (next == '\n')
-		    display(world);
-	}while(next == '\n');
+            display(world);
+    }while(next == '\n');
 
-	return 0;
+    return 0;
 }
 
 void createWorld(char world[][COLUMN])
 {
     srand(time(NULL));
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < COLUMN; j++)
-		{
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COLUMN; j++)
+        {
             world[i][j] = ((rand() % 2 + 3) *10) + 2; //Returns ' ' or '*'
             cout<<world[i][j];
-		}
+        }
         cout<<endl;
-	}
+    }
 }
 
 void nextGeneration(char world[][COLUMN])
 {
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < COLUMN; j++)
-		{
-			if (isOccupied(world[i][j]) && (neighborCount(world,i, j) == 0 || neighborCount(world, i, j) == 1))
-				world[i][j] = ' ';
-			else if (isOccupied(world[i][j]) && (neighborCount(world, i, j) > 3))
-				world[i][j] = ' ';
-			else if (isEmpty(world[i][j]) && (neighborCount(world, i, j) == 3))
-				world[i][j] = '*';
-		}
-	}
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COLUMN; j++)
+        {
+            if (isOccupied(world[i][j]) && (neighborCount(world,i, j) == 0 || neighborCount(world, i, j) == 1))
+                world[i][j] = ' ';
+            else if (isOccupied(world[i][j]) && (neighborCount(world, i, j) > 3))
+                world[i][j] = ' ';
+            else if (isEmpty(world[i][j]) && (neighborCount(world, i, j) == 3))
+                world[i][j] = '*';
+        }
+    }
 }
 
 int neighborCount(char world[][COLUMN], int row, int column)
 {
-	int count = 0;
+    int count = 0;
 
-	if (row != 0 && world[row-1][column] == '*')
-		++count;
-	if ((row != 0 && column != (COLUMN - 1)) && world[row-1][column+1] == '*')
-		++count;
-	if (column != (COLUMN - 1) && world[row][column+1] == '*')
-		++count;
-	if (row != (ROW - 1) && world[row+1][column] == '*')
-		++count;
-	if ((row != (ROW - 1) && column != (COLUMN - 1)) && world[row+1][column+1] == '*')
-		++count;
-	if ((row != 0 && column != 0) && world[row-1][column-1] == '*')
-		++count;
-	if (column != 0 && world[row][column-1] == '*')
-		++count;
-	if ((row != (ROW - 1) && column != 0) && world[row+1][column-1] == '*')
-		++count;
+    if (row != 0 && world[row-1][column] == '*')
+        ++count;
+    if ((row != 0 && column != (COLUMN - 1)) && world[row-1][column+1] == '*')
+        ++count;
+    if (column != (COLUMN - 1) && world[row][column+1] == '*')
+        ++count;
+    if (row != (ROW - 1) && world[row+1][column] == '*')
+        ++count;
+    if ((row != (ROW - 1) && column != (COLUMN - 1)) && world[row+1][column+1] == '*')
+        ++count;
+    if ((row != 0 && column != 0) && world[row-1][column-1] == '*')
+        ++count;
+    if (column != 0 && world[row][column-1] == '*')
+        ++count;
+    if ((row != (ROW - 1) && column != 0) && world[row+1][column-1] == '*')
+        ++count;
 
-	return count;
+    return count;
 }
 
 bool isEmpty(char cell)
 {
-	return (cell == ' ');
+    return (cell == ' ');
 }
 
 bool isOccupied(char cell)
 {
-	return (cell == '*');
+    return (cell == '*');
 }
 
 void display(char world [][COLUMN])
 {
     cout<<"\n\n\n\n";
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < COLUMN; j++)
-			cout<<world[i][j];
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COLUMN; j++)
+            cout<<world[i][j];
         cout<<endl;
-	}
+    }
 }
 
 bool worldIsEmpty(char world[][COLUMN])
 {
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < COLUMN; j++)
-			if (world[i][j] == '*')
-				return false;
-	}
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COLUMN; j++)
+            if (world[i][j] == '*')
+                return false;
+    }
 
-	return true;
+    return true;
 }
 
 bool worldIsFull(char world[][COLUMN])
 {
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < COLUMN; j++)
-			if (world[i][j] == ' ')
-				return false;
-	}
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COLUMN; j++)
+            if (world[i][j] == ' ')
+                return false;
+    }
 
-	return true;
+    return true;
 }
